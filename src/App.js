3d564164ce2d589dd32fd9d1bdb3f2c3265f8ff5
ambegossi <andersonbegossi@gmail.com>
2020-01-24 11:10:09 -0300

    Finished features

--- src/App.js
+++ src/App.js
@@ -1,103 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevForm from "./components/DevForm";
+import DevItem from "./components/DevItem";
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs(); //precisamos chamar a função, para executar assim que useEffect for executado
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/32908189?s=460&v=4"
-                alt="Anderson Begossi"
-              />
-              <div className="user-info">
-                <strong>Anderson Begossi</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>think twice code once.</p>
-            <a href="https://github.com/ambegossi">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/32908189?s=460&v=4"
-                alt="Anderson Begossi"
-              />
-              <div className="user-info">
-                <strong>Anderson Begossi</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>think twice code once.</p>
-            <a href="https://github.com/ambegossi">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/32908189?s=460&v=4"
-                alt="Anderson Begossi"
-              />
-              <div className="user-info">
-                <strong>Anderson Begossi</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>think twice code once.</p>
-            <a href="https://github.com/ambegossi">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/32908189?s=460&v=4"
-                alt="Anderson Begossi"
-              />
-              <div className="user-info">
-                <strong>Anderson Begossi</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>think twice code once.</p>
-            <a href="https://github.com/ambegossi">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
